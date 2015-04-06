b465d2cdbfbb83f75998d427a8aec980a4f4ffa1
ayush <ayushtiwariiitr@gmail.com>
2015-04-06 20:00:47 +0530

    Multiply matrix function developed

--- practical5.py
+++ practical5.py
@@ -5,29 +5,41 @@ import numpy
 
 def enter_matrix():
     """
-        Function to get a matrix using raw_input of n no. of
-        rows and m no. of columns
+        Function to get a matrix using raw_input of m no. of
+        rows and n no. of columns
     """
 
-    n = int(raw_input("Enter n, i.e, number of rows:"))
-    m = int(raw_input("Enter m, i.e, number of columns:"))
+    m = int(raw_input("Enter n, i.e, number of rows:"))
+    n = int(raw_input("Enter m, i.e, number of columns:"))
 
     matrix = []
-    for i in xrange(n):
+    for i in xrange(m):
         row = []
-        for j in xrange(m):
+        for j in xrange(n):
             row.append(float(raw_input()))
         matrix.append(row)
 
     return matrix
 
-def multiply_matrix(matrix1, matrix2):
+def multiply_matrix(matrix1=[[1, 2, 3], [1,0, 0]], matrix2=[[1,1,1], [1,1,1], [1,1,2]]):
     """
         Function to multiply two matrix with each other and
         return a final matrix. This takes two matrix as argument.
         The datatype of the matrix would be a 2-D list/array.
     """
 
-    for i in matrix1:
-        for j in i:
-            pass
+    if not len(matrix1[0])==len(matrix2):
+        return "Matrices are not multiplicable"
+
+
+    multiplication_matrix = []
+    for k in xrange(len(matrix1)):
+        row = []
+        for i in xrange(len(matrix2)):
+            point = 0
+            for j in xrange(len(matrix1[k])):
+                point = point + matrix1[k][j]*matrix2[j][i]
+            row.append(point)
+        multiplication_matrix.append(row)
+
+    return multiplication_matrix
